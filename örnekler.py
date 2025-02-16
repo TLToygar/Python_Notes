63c0818487fd13a23c990c36b45a1ee5d69d898f
TLToygar <cosartoygar@gmail.com>
2025-02-16 16:23:33 +0300

    added aracyakit example

--- örnekler.py
+++ örnekler.py
@@ -6,14 +6,46 @@
 #6-listenin sonuna ford ve citroen ekle 
 #7-listenin son elemanını sil
 
-markalar = ["toyota", "bmw", "renault", "mercedes"]
-print(len(markalar))
-print(markalar)
-print(markalar[0], markalar[-1])
-markalar[2] = "TOGG"
-print(markalar)
-print(markalar[:2])
-markalar= markalar + ["ford", "citroen"]
-print(markalar)
-markalar.pop()
-print(markalar)
+#markalar = ["toyota", "bmw", "renault", "mercedes"]
+#print(len(markalar))
+#print(markalar)
+#print(markalar[0], markalar[-1])
+#markalar[2] = "TOGG"
+#print(markalar)
+#print(markalar[:2])
+#markalar= markalar + ["ford", "citroen"]
+#print(markalar)
+#markalar.pop()
+#print(markalar)
+#Yakitlar {
+ #   "benzin": 46.54,
+  #  "lpg": 27.62,
+   # "dizel": 46.61
+#}
+#Kosul ifadeleri - if, elif, else
+
+
+#Sorular:
+#1- Bir aracın (yakıt tipine göre) klavyeden belirtilen bir mesafede ne kadar yakıt tükkettiğini hesaplayan 
+# uygulamayı yaz.
+benzin = 46.54
+lpg = 27.62
+dizel = 46.61
+
+yakit_tipi = input("Yakıt tipini giriniz: ")
+tüketim = float(input("100Km'deki tüketimi giriniz: ")) 
+mesafe = int(input("Mesafeyi giriniz Km: "))
+
+kmBasinaYakilanYakit =  (tüketim / 100) 
+toplamYakitTüketim = kmBasinaYakilanYakit * mesafe
+toplamYakitTüketim = round(toplamYakitTüketim, 2)    
+print("Toplam Yakıt Tüketimi: " + str(toplamYakitTüketim))
+if yakit_tipi == "benzin":
+    print("Ödemeniz gereken miktar: " + str(toplamYakitTüketim * benzin))
+elif yakit_tipi == "lpg":
+    print("Ödemeniz gereken miktar: " + str(toplamYakitTüketim * lpg))
+elif yakit_tipi == "dizel":
+    print("Ödemeniz gereken miktar: " + str(toplamYakitTüketim * dizel))
+else:
+    print("Geçersiz yakıt tipi")
+    
